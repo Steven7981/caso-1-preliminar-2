11b6946bf9e6a71caaf5322819dd72a97a9dc2f6
Steven7981 <stevenfengwu79@gmail.com>
2023-09-03 21:16:26 -0600

    Create preliminar2.cpp

---
+++ preliminar2.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+const int TAMANO=9999;
+
+std::string insertar(std::string arrays[TAMANO],std:: string dato, int posicion){
+    int contador=0;
+    for (int i = 0;i<TAMANO;i++){
+        if (!arrays[i].empty()){
+            contador++;
+        }
+        else break;
+    }
+
+    if (posicion == contador+1){
+        if (arrays[posicion+1].empty()){
+            arrays[posicion+1]=arrays[posicion];
+            arrays[posicion]=dato;
+        }
+        for (int i = 0;i<TAMANO;i++){
+        if (!(*arrays).empty()){
+            std:: cout << arrays[i];
+        }
+        else break;
+    }
+    }
+    if (arrays[posicion].empty()){
+        arrays[posicion]=dato;
+        for (int i = 0;i<TAMANO;i++){
+        if (!arrays[i].empty()){
+            std:: cout << arrays[i]<<" ";
+    }
+}
+}
+    if (!arrays[posicion].empty()){
+        int i = contador-1;
+        while (i>=posicion){
+            arrays[i+1]=arrays[i];
+            i--;
+        }
+        arrays[posicion]=dato;
+}
+}
+
+void agregar(std::string arrays[TAMANO],std:: string dato)
+    {
+        int contador=0;
+        for (int i = 0;i<TAMANO;i++){
+        if (!arrays[i].empty()){
+            contador++;
+        }
+        else break;
+    }
+        arrays[contador+1]=dato;
+    }
+
+std::string borrar(std::string arrays[TAMANO],std::string palabra){
+    std::string palabra_borrar= palabra;
+    int contador=0;
+    for (int i = 0;i<TAMANO;i++){
+        if (!arrays[i].empty()){
+            contador++;
+        }
+        else break;
+        
+        }
+    for (int i = 0; i<contador;i++){
+        if (arrays[i].find(palabra_borrar) != std::string::npos) {
+            for (int j = i; j < contador - 1; j++) {
+                arrays[j] = arrays[j + 1];
+                arrays[j+1].erase();
+            }
+            contador--; 
+            i--;
+    }
+    }
+    std::cout<<" nuevo ";
+    for (int i = 0; i<5;i++){
+        if (arrays[i].empty()){
+            std::cout<<" no "<<std::endl;
+        }
+        else {
+            std::cout<< arrays[i]<<i<< std:: endl;
+        }
+        }
+    
+    return ("Se eliminaron todas las noticias que contenia la palabra "+ palabra);
+}
