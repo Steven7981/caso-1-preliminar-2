500160cdbca5f8f74b76fd8d625e7f2122c24cfe
Steven7981 <stevenfengwu79@gmail.com>
2023-09-04 14:14:12 -0600

    Update preliminar2_main.cpp

--- preliminar2_main.cpp
+++ preliminar2_main.cpp
@@ -3,10 +3,14 @@
 
 std::string eliminarNoticia(std::string array[9999],std::string palabra){
     std::string x = borrar(array,palabra);
+    return ("Se eliminaron todas las noticias que contiene la palabra "+ x);
+
 }
 
 std::string ordenarNoticias(std::string array [9999],int posicion, int posicion_mover){
     std::string auxiliar = insertar(array, array[posicion], posicion-posicion_mover);
+    std::string x = std::to_string(posicion_mover);
+    return ("Se ordenó la noticia a la posicion ") + x;
 }
 void showTop5(std::string array[9999]){
     for (int i =0; i<5;i++){
@@ -15,9 +19,9 @@ void showTop5(std::string array[9999]){
 }
 
 int main(){
-    std::string news[99999] = {"hola","que tal","hola que","tal que", "no se"};
+    std::string news[99999] = {"hola","que tal","hola que","tal que", "no se"," no deberia","dfdf"};
 
-    showTop5(news);
+    mostrar_todo(news,"hola");
     /*std::string x= borrar(news,"que");
     std::cout<<x;
    for (int i = 0; i < TAMANO; i++) {
