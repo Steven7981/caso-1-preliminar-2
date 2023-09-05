2324ea47f349a5e5d301a0d8feb7c01c6e7dd16a
Steven7981 <stevenfengwu79@gmail.com>
2023-09-05 14:53:08 -0600

    Update preliminar2_main.cpp

--- preliminar2_main.cpp
+++ preliminar2_main.cpp
@@ -17,18 +17,3 @@ void showTop5(std::string array[9999]){
         std::cout<<mostrar(array,i)<<std:: endl;
     }
 }
-
-int main(){
-    std::string news[99999] = {"hola","que tal","hola que","tal que", "no se"," no deberia","dfdf"};
-
-    mostrar_todo(news,"hola");
-    /*std::string x= borrar(news,"que");
-    std::cout<<x;
-   for (int i = 0; i < TAMANO; i++) {
-        if (!news[i].empty()) {
-            std::cout << "Noticia " << i << ": " << news[i] << std::endl;
-        }*/
-    return 0;
-}
-
-
